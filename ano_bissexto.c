0158db88f9a15feebad71035603bd5ad6e31342e
Geovanna Ribeiro <geovanna.ribeiro0412@gmail.com>
2024-09-11 12:50:16 +0000

    Desvio de fluxo simples. Ano bissexto

---
+++ ano_bissexto.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main(){
+
+    int ano;
+    printf("\nDigite um Ano:\n\n");
+    scanf("%d",&ano);
+
+    if (ano % 4 ==0)
+    {
+        printf("\nEste Ano é bissexto!\n\n");
+
+    }
+    
+}
