b0d66628bd1d4143de8bbd99c5c6027da707222d
Geovanna Ribeiro <geovanna.ribeiro0412@gmail.com>
2024-09-12 13:05:16 +0000

    Usando o switch para saber qual dia do rodízio de acordo com final da placa

---
+++ rodizio_switch.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(){
+    int final_placa;
+
+printf("\n Digite o número final da sua placa\n\n");
+scanf("%d",&final_placa);
+
+    switch(final_placa){
+
+        case 1:
+        printf("\nRodízio na Segunda-feira\n\n");
+    break;
+    
+        case 2:
+        printf("\nRodízio na Segunda-feira\n\n");
+    break;
+
+        case 3:
+        printf("\nRodízio na Terça-feira\n\n");
+    break;
+
+        case 4:
+        printf("\nRodízio na Terça-feira\n\n");
+    break;
+
+        case 5:
+        printf("\nRodízio na Quarta-feira\n\n");
+    break;
+
+        case 6:
+        printf("\nRodízio na Quarta-feira\n\n");
+    break;
+
+        case 7:
+        printf("\nRodízio na Quinta-feira\n\n");
+    break;
+
+        case 8:
+        printf("\nRodízio na Quinta-feira\n\n");
+    break;
+
+        case 9:
+        printf("\nRodízio na Sexta-feira\n\n");
+    break;
+
+        case 0:
+        printf("\nRodízio na Sexta-feira\n\n");
+    break;
+    
+    }
+}
