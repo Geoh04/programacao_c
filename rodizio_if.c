865cdc58f45ba73054636957a3a704fbdb768076
Geovanna Ribeiro <geovanna.ribeiro0412@gmail.com>
2024-09-12 12:28:42 +0000

    Usando o if e else várias vezes para ver o rodio de carro

---
+++ rodizio_if.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main(){
+
+int final_placa;
+
+printf("\n Digite o número final da sua placa\n\n");
+scanf("%d",&final_placa);
+
+if(final_placa==1 ){
+
+    printf("\nVocê não pode sair com o veículo as segunda-feira\n\n");
+}
+
+else if(final_placa==2){
+
+    printf("\nVocê não pode sair com o veículo as segunda-feira\n\n");
+}
+
+else if(final_placa==3){
+        
+    printf("\nVocê não pode sair com o veículo as terça-feira\n\n");
+}
+
+else if(final_placa==4){
+        
+    printf("\nVocê não pode sair com o veículo as terça-feira\n\n");
+}
+
+else if(final_placa==5){
+        
+    printf("\nVocê não pode sair com o veículo as quarta-feira\n\n");
+}
+
+else if(final_placa==6){
+        
+    printf("\nVocê não pode sair com o veículo as quarta-feira\n\n");
+}
+
+else if(final_placa==7){
+        
+    printf("\nVocê não pode sair com o veículo as quinta-feira\n\n");
+}
+
+else if(final_placa==8){
+        
+    printf("\nVocê não pode sair com o veículo as quinta-feira\n\n");
+}
+
+else if (final_placa==9){
+        
+    printf("\nVocê não pode sair com o veículo as sexta-feira\n\n");
+}
+
+else if(final_placa==0){
+        
+    printf("\nVocê não pode sair com o veículo as sexta-feira\n\n");
+}
+
+else{
+    printf("\nFinal de placa inexistente! Tente novamente\n\n");
+}
+
+}
